413f46737ef7221867dca03fc09efe46fb1fd0bf
sahithh <sahithh.gudiyella9@gmail.com>
2023-04-12 21:34:25 -0400

    changes

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "public_ip" {
   value = aws_instance.my-instance.public_ip
 }
+
+output "sg_id"{
+  value = aws_security_group.my-instance-sg.id
+}
+output "instance_id"{
+  value = aws_instance.my-instance.id
+}
